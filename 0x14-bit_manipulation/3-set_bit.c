568444d5430176e79ece71cf974fd7e530c7d3e6
nenojackline <chepngenojackline0@gmail.com>
2023-04-07 03:16:37 -0700

    3-set_bit.c 4-clear_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,12 +9,12 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int num;
+	unsigned long int mask;
 
 	if (index >= (sizeof(*n) * 8))
 		return (-1);
-	num = 1;
-	*n = *n | (num << index);
+	mask = 1;
+	*n = *n | (mask << index);
 
 	return (1);
 }
--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,14 +9,14 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int num;
+	unsigned long int mask;
 
 	if (index >= (sizeof(*n) * 8))
 		return (-1);
-	num = 1;
-	num = num << index;
-	num = ~num;
-	*n = *n & num;
+	mask = 1;
+	mask = mask << index;
+	mask = ~mask;
+	*n = *n & mask;
 
 	return (1);
 }
