f81b4432b84b27830c56ca4894eed3e9ba402147
nenojackline <chepngenojackline0@gmail.com>
2023-05-08 13:16:20 -0700

    modify files

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -5,7 +5,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+Jacque
 void check_elf(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
 void print_class(unsigned char *e_ident);
