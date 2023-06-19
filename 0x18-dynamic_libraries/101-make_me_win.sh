055d55758b5afbcc681ecbaa03e981f3dda778e4
nenojackline <chepngenojackline0@gmail.com>
2023-06-19 14:19:55 -0700

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/debugulize/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libtest.so
-export LD_PRELOAD=/tmp/libtest.so
+wget https://raw.githubusercontent.com/nenojackline/alx-low_level_programming/master/0x18-dynamic_libraries/litest.so libtest.so
+export LD_PRELOAD="./litest.so"
